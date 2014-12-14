371837e2c95be91cba4492c4aab0b1a9e504c45c
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2014-12-14 02:13:55 +0100

    Dodana možnost tihega delovanja.

--- clearpdf.r
+++ clearpdf.r
@@ -1,19 +1,25 @@
+if ("silent" %in% ls() && silent) {
+  dog <- function (...) {}
+} else {
+  dog <- cat
+}
+
 delete <- dir(pattern = "\\.pdf$", recursive = TRUE, ignore.case = TRUE)
 if (length(delete) > 0) {
-  cat("Pobrisane bodo sledeče datoteke:\n", delete, "\n")
+  dog("Pobrisane bodo sledeče datoteke:\n", delete, "\n")
   if(readline("Nadaljujem? [da/NE]") == 'da') {
     success <- file.remove(delete)
     if (any(!success)) {
-      cat("Sledeče datoteke so pobrisane:\n", delete[success], "\n")
-      cat("Sledeče datoteke NISO pobrisane:\n", delete[!success], "\n")
+      dog("Sledeče datoteke so pobrisane:\n", delete[success], "\n")
+      dog("Sledeče datoteke NISO pobrisane:\n", delete[!success], "\n")
     } else {
-      cat("Datoteke so pobrisane.\n")
+      dog("Datoteke so pobrisane.\n")
     }
   } else {
-    cat("Datoteke niso pobrisane.\n")
+    dog("Datoteke niso pobrisane.\n")
   }
 } else {
-  cat("Ne najdem nobene datoteke PDF.\n")
+  dog("Ne najdem nobene datoteke PDF.\n")
 }
-cat("Brišem delovno okolje.\n")
+dog("Brišem delovno okolje.\n")
 rm(list = ls())
