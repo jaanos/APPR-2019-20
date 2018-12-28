78c44c26516b3b4e5456188af3e1f832bb85fbc3
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2018-12-28 14:32:39 +0100

    Uporabi pot do datoteke SHP

--- lib/uvozi.zemljevid.r
+++ lib/uvozi.zemljevid.r
@@ -50,7 +50,7 @@ uvozi.zemljevid <- function(url, ime.zemljevida, pot.zemljevida="",
                                       function(x)
                                         paste(c(x[1:(length(x)-1)], tolower(x[length(x)])),
                                               collapse="."))))
-  zemljevid <- readOGR(pot, ime.zemljevida)
+  zemljevid <- readOGR(shp, ime.zemljevida)
 
   if (!is.null(encoding)) {
     loc <- locale(encoding=encoding)
