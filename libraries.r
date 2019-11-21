dbdd450b4dde8d4faa1d0807f777048cd12fe549
mperbil <49915442+mperbil@users.noreply.github.com>
2019-11-21 18:40:28 +0100

    Add files via upload

---
+++ libraries.r
@@ -0,0 +1,10 @@
+library(knitr)
+library(rvest)
+library(gsubfn)
+library(tidyr)
+library(shiny)
+
+options(gsubfn.engine="R")
+
+# Uvozimo funkcije za pobiranje in uvoz zemljevida.
+source("lib/uvozi.zemljevid.r", encoding="UTF-8")
