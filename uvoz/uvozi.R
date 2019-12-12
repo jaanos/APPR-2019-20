4b75c714964604d5a9156828aa39ec5887e844a4
Tim Kalan <timkalan99@gmail.com>
2019-12-12 15:12:05 +0100

    Urejanje tabele

--- uvoz/uvozi.R
+++ uvoz/uvozi.R
@@ -1,8 +1,15 @@
 library(readr)
 library(dplyr)
 
-# netoMigracija <- read_csv("podatki/migrant_stock.csv") %>% 
-#   select(-"1961", -"1962", -"1963") # to maš še ful za zbrisat
-
-migracija <- read_csv("podatki/migracija.csv") %>%
+# UREJANJE GLAVNE TABELE O MIGRACIJI
+migracija <- read_csv("podatki/migracija.csv", n_max = 160776, na = c("..")) %>%
   select(-"Migration by Gender Code", -"Country Origin Code", -"Country Dest Code")
+
+colnames(migracija) <- c("origin_country", "migration_gender", "dest_country", "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2010")
+
+
+
+
+
+# BDP podatki iz wikipedije
+# neto migracija iz fajla ali izracunano?
