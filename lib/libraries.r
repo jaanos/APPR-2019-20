10d305869564c834b16c5f43b419b43e43f5fcbd
Lenart11 <49915334+Lenart11@users.noreply.github.com>
2019-12-12 08:36:54 +0100

    Update libraries.r

--- lib/libraries.r
+++ lib/libraries.r
@@ -3,6 +3,6 @@ library(rvest)
 library(gsubfn)
 library(tidyr)
 library(shiny)
-
+options(gsubfn.engine="R")
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
