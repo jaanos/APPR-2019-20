a05c25106d3c767c439ad59ffcbd2e5e5989bc77
Tim Kalan <timkalan99@gmail.com>
2019-12-12 16:13:14 +0100

    Glavna tabela se bliža tidy data

--- uvoz/uvozi.R
+++ uvoz/uvozi.R
@@ -1,15 +1,21 @@
 library(readr)
 library(dplyr)
+library(tidyr)
 
 # UREJANJE GLAVNE TABELE O MIGRACIJI
 migracija <- read_csv("podatki/migracija.csv", n_max = 160776, na = c("..")) %>%
   select(-"Migration by Gender Code", -"Country Origin Code", -"Country Dest Code")
 
-colnames(migracija) <- c("origin_country", "migration_gender", "dest_country", "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2010")
+colnames(migracija) <- c("origin_country", "gender", "dest_country", "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2010")
 
+skupno <- migracija %>% filter(gender=="Total") %>% select(-"gender") %>% 
+  gather(decade, number, "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2010") %>%
+  arrange(origin_country)
 
-
-
+poSpolih <- migracija %>% filter(gender=="Female" | gender=="Male" ) %>%
+  gather(decade, number, "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2010") %>%
+  arrange(origin_country)
+poSpolih <- poSpolih[c(1,3,2,4,5)]
 
 # BDP podatki iz wikipedije
 # neto migracija iz fajla ali izracunano?
