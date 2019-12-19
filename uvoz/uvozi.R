0c362800023fb223a05fd7574844e1ec741f2c91
Tim Kalan <timkalan99@gmail.com>
2019-12-19 10:02:51 +0100

    začel s podatki s spleta

--- uvoz/uvozi.R
+++ uvoz/uvozi.R
@@ -1,12 +1,16 @@
 library(readr)
 library(dplyr)
 library(tidyr)
+library(rvest)
+library(XML)
+library(gsubfn)
 
 # UREJANJE GLAVNE TABELE O MIGRACIJI
 migracija <- read_csv("podatki/migracija.csv", n_max = 160776, na = c("..")) %>%
   select(-"Migration by Gender Code", -"Country Origin Code", -"Country Dest Code")
 
-colnames(migracija) <- c("origin_country", "gender", "dest_country", "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2010")
+colnames(migracija) <- c("origin_country", "gender", "dest_country", 
+                         "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2010")
 
 skupno <- migracija %>% filter(gender=="Total") %>% select(-"gender") %>% 
   gather(decade, number, "1960-1969", "1970-1979", "1980-1989", "1990-1999", "2000-2010") %>%
@@ -18,4 +22,15 @@ poSpolih <- migracija %>% filter(gender=="Female" | gender=="Male" ) %>%
 poSpolih <- poSpolih[c(1,3,2,4,5)]
 
 # BDP podatki iz wikipedije
+url <- "https://en.wikipedia.org/wiki/List_of_countries_by_past_and_projected_GDP_(PPP)"
+stran <- read_html(url)
+
+drzave <- stran %>% 
+  html_nodes(xpath = "//table[@class = 'sortable wikitable jquery-tablesorter']") %>%
+  html_text
+
+
+
+
+
 # neto migracija iz fajla ali izracunano?
