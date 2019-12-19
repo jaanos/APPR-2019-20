f08176a9c54fa6fd9d87ee909b6fd117469af695
Tim Kalan <timkalan99@gmail.com>
2019-12-19 19:24:42 +0100

    spletni podatki tidy (takšna pa morda ni še koda)

--- uvoz/uvozi.R
+++ uvoz/uvozi.R
@@ -29,14 +29,28 @@ stran <- read_html("podatki/bdp.html")
 
 bdpji <- stran %>%
   html_nodes(xpath = "//table[@class='sortable wikitable']//td") %>%
-  html_text() %>% str_replace_all(',', '') # ene majo dve vejici fakej
+  html_text() %>% str_replace_all(',', '')
 
 bdp <- as.data.frame(matrix(bdpji, ncol = 11, byrow = TRUE))
-bdp <- data.frame(lapply(bdp, as.character), stringsAsFactors=FALSE) 
+bdp <- data.frame(lapply(bdp, as.character), stringsAsFactors=FALSE)
 bdp <- bdp[1:768, ]
 bdp[bdp==""] <- NA
-
 bdp[, 2:11] <- sapply(bdp[, 2:11], as.numeric)
 
+osemdeseta <- bdp[1:192,]
+colnames(osemdeseta) <- c("drzave", 1980:1989)
+devetdeseta <- bdp[193:384,]
+colnames(devetdeseta) <- c("drzave", 1990:1999)
+dvatisoca <- bdp[385:576,]
+colnames(dvatisoca) <- c("drzave", 2000:2009)
+dvadeseta <- bdp[577:768,]
+colnames(dvadeseta) <- c("drzave", 2010:2019)
+
+osemdeseta <- osemdeseta %>% gather(leto, BDP, "1980":"1989")
+devetdeseta <- devetdeseta %>% gather(leto, BDP, "1990":"1999")
+dvatisoca <- dvatisoca %>% gather(leto, BDP, "2000":"2009")
+dvadeseta <- dvadeseta %>% gather(leto, BDP, "2010":"2019")
+
+bdp <- rbind(osemdeseta, devetdeseta, dvatisoca, dvadeseta)
 
 # neto migracija iz fajla ali izracunano?
