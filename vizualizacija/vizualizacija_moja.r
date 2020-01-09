7335a81bebd56b4b9caa35c71ec663644af0a974
mperbil <49915442+mperbil@users.noreply.github.com>
2020-01-09 21:59:33 +0100

    Add files via upload

--- vizualizacija/vizualizacija_moja.r
+++ vizualizacija/vizualizacija_moja.r
@@ -15,39 +15,47 @@ library(ggvis)
 #  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
 
 ######
-ggplot(tabela_1) + aes(x=igralec, y=cleanSheet) + geom_point()
-ggplot(tabela_1) + aes(x=appearances, y=cleanSheet, color=igralec) + geom_point()+ geom_text(aes(label=igralec),hjust=0, vjust=0) + ggtitle("Tekme brez prejetega zadetka")
-ggplot(tabela_2) + aes(x=penaltyFaced, y=penaltySave, color=igralec) + geom_point() + ggtitle("Posredovanje pri enajstmetrovkah")
-ggplot(tabela_2) + aes(x=savedShotsFromInsideTheBox, y=goalsConcededInsideTheBox, color=igralec) + geom_point() + ggtitle("Posredovanje pri strelih znotraj kazenskega prostora")
-ggplot(tabela_2) + aes(x=savedShotsFromOutsideTheBox, y=goalsConcededOutsideTheBox, color=igralec) + geom_point() + ggtitle("Posredovanje pri strelih izven kazenskega prostora")
-ggplot(tabela_3) + aes(x=runsOut, y=successfulRunsOut, color=igralec) + geom_point() + ggtitle("Iztekanja")
-ggplot(tabela_3) + aes(x=highClaims, y=crossesNotClaimed, color=igralec) + geom_point() + ggtitle("Posredovanje pri predlo≈ækih")
-ggplot(tabela_4) + aes(x=igralec, y=totalPasses, size=accuratePassesPercentage) + geom_point() + ggtitle("Podaje")
-ggplot(tabela_4) + aes(x=igralec, y=accurateLongBalls, size=accurateLongBallsPercentage) + geom_point() + ggtitle("Dolge ≈æoge")
+ggplot(t1) + aes(x=igralec, y=cleanSheet) + geom_bar(stat="identity")
+ggplot(t1) + aes(x=appearances, y=cleanSheet, color=drzava) + geom_point()+ geom_text(aes(label=igralec),hjust=0, vjust=0) + ggtitle("Tekme brez prejetega zadetka")
+ggplot(t2) + aes(x=penaltyFaced, y=penaltySave, color=drzava) + geom_point() + geom_text(aes(label=igralec),hjust=0, vjust=0) + ggtitle("Posredovanje pri enajstmetrovkah")
+ggplot(t2) + aes(x=savedShotsFromInsideTheBox, y=goalsConcededInsideTheBox, color=drzava) + geom_point() + geom_text(aes(label=igralec),hjust=0, vjust=0) + ggtitle("Posredovanje pri strelih znotraj kazenskega prostora")
+ggplot(t2) + aes(x=savedShotsFromOutsideTheBox, y=goalsConcededOutsideTheBox, color=drzava) + geom_point() + geom_text(aes(label=igralec),hjust=0, vjust=0) + ggtitle("Posredovanje pri strelih izven kazenskega prostora")
+ggplot(t3) + aes(x=runsOut, y=successfulRunsOut, color=drzava) + geom_point() + geom_text(aes(label=igralec),hjust=0, vjust=0) + ggtitle("Iztekanja")
+ggplot(t3) + aes(x=highClaims, y=crossesNotClaimed, color=igralec, shape=drzava) + geom_point() + ggtitle("Posredovanje pri predlo≈ækih")
+ggplot(t4) + aes(x=igralec, y=totalPasses, size=accuratePassesPercentage) + geom_point() + ggtitle("Podaje")
+ggplot(t4) + aes(x=igralec, y=accurateLongBalls, size=accurateLongBallsPercentage) + geom_point() + ggtitle("Dolge ≈æoge")
 
-ggplot(podatki_v_ap_cs_ita) + aes(x=igralec, y=cleanSheet, color=appearances) + geom_point()
-ggplot(podatki_v_ap_cs_ita) + aes(x=appearances, y=cleanSheet, color=igralec) + geom_point()
+t5 <- t1 %>% group_by(drzava) %>% summarise(cleanSheet_drzava=sum(cleanSheet))
+ggplot(t5) + aes(x=drzava, y=cleanSheet_drzava) + geom_bar(stat="identity")
 
-#ggplot(podatki_v_ap_cs_ita) + aes(x=factor(1), fill=igralec) + geom_bar(width=1) +
- # coord_polar(theta="y") + xlab("") + ylab("")
+t6 <- t2 %>% group_by(drzava) %>% summarise(penaltySave_drzava=sum(penaltySave))
+ggplot(t6) + aes(x=drzava, y=penaltySave_drzava) + geom_bar(stat="identity")
 
-#tortni diagram za ita, za cleansheete 
-bp<- ggplot(podatki_v_ap_cs_ita, aes(x="", y=cleanSheet, fill=igralec))+
-  geom_bar(width = 1, stat = "identity")
-bp
-pie <- bp + coord_polar("y", start=0)
-pie
+
+#ggplot(t1, aes(x=cleanSheet)) + geom_histogram() +
+#  ggtitle("Pogostost ötevila naselij") + xlab("ätevilo naselij") + ylab("ätevilo obËin")
+
+#ggplot(podatki_v_ap_cs_ita) + aes(x=igralec, y=cleanSheet, color=appearances) + geom_point()
+#ggplot(podatki_v_ap_cs_ita) + aes(x=appearances, y=cleanSheet, color=igralec) + geom_point()
+
+
+# #tortni diagram za ita, za cleansheete 
+# bp<- ggplot(podatki_v_ap_cs_ita, aes(x="", y=cleanSheet, fill=igralec))+
+#   geom_bar(width = 1, stat = "identity")
+# bp
+# pie <- bp + coord_polar("y", start=0)
+# pie
 #za vse
-bp<- ggplot(tabela_1, aes(x="", y=cleanSheet, fill=igralec))+
-  geom_bar(width = 1, stat = "identity")
+bp<- ggplot(t1, aes(x="", y=cleanSheet, fill=igralec))+
+ geom_bar(width = 1, stat = "identity")
 bp
 pie <- bp + coord_polar("y", start=0)
 pie
 
+#z <- ggplot(zemljevid, aes(x=long, y=lat))
 # Uvozimo zemljevid.
 zemljevid <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/110m_cultural.zip",
                              "ne_110m_admin_0_countries", encoding="UTF-8")
-#z <- ggplot(zemljevid, aes(x=long, y=lat))
 
 tm_shape(merge(zemljevid,
                t1 %>% group_by(drzava) %>% summarise(cleanSheet=sum(cleanSheet)),
