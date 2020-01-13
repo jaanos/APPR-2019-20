7464e9aaf91545436086454453afaaae63e6194c
Tim Kalan <timkalan99@gmail.com>
2020-01-13 22:39:41 +0100

    proof of concept za poročilo

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,3 +1,6 @@
+# lahko združiš podatke o državah v eno samo tabelo 
+
+
 # UREJANJE GLAVNE TABELE O MIGRACIJI
 migracija <- read_csv("podatki/migracija.csv", n_max = 160776, na = c("..")) %>%
   select(-"Migration by Gender Code", -"Country Origin Code", -"Country Dest Code")
@@ -56,7 +59,7 @@ colnames(pop) <- c("country", 1950:2020)
 pop[, 2:72] <- as.data.frame(apply(pop[, 2:72],2,function(x)gsub('\\s+', '',x)))
 pop[, 1:72] <- sapply(pop[, 1:72], as.character)
 pop[, 2:72] <- sapply(pop[, 2:72], as.numeric)
-pop <- arrange(pop, drzava)
+pop <- arrange(pop, country)
 pop <- pop[, c(1, 12:62)]
 pop <- pop %>% gather(leto, populacija, "1960":"2010")
 pop$populacija <- pop$populacija * 1000
@@ -69,3 +72,65 @@ relig <- read.csv("podatki/religije.csv") %>%
 relig[, 1] <- sapply(relig[, 1], as.character)
 
 rm(devetdeseta, dvadeseta, dvatisoca, migracija, osemdeseta, stran, bdpji, url)
+
+#tisti placeholder
+svn <- relig %>% filter(country == "Slovenia") %>%
+  select(-"country") %>%
+  gather(religion, number)
+
+
+# # 2. faza: Uvoz podatkov
+# 
+# sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+# 
+# # Funkcija, ki uvozi občine iz Wikipedije
+# uvozi.obcine <- function() {
+#   link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
+#   stran <- html_session(link) %>% read_html()
+#   tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+#     .[[1]] %>% html_table(dec=",")
+#   for (i in 1:ncol(tabela)) {
+#     if (is.character(tabela[[i]])) {
+#       Encoding(tabela[[i]]) <- "UTF-8"
+#     }
+#   }
+#   colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
+#                         "ustanovitev", "pokrajina", "regija", "odcepitev")
+#   tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
+#   tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
+#   tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
+#   for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
+#     if (is.character(tabela[[col]])) {
+#       tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
+#     }
+#   }
+#   for (col in c("obcina", "pokrajina", "regija")) {
+#     tabela[[col]] <- factor(tabela[[col]])
+#   }
+#   return(tabela)
+# }
+# 
+# # Funkcija, ki uvozi podatke iz datoteke druzine.csv
+# uvozi.druzine <- function(obcine) {
+#   data <- read_csv2("podatki/druzine.csv", col_names=c("obcina", 1:4),
+#                     locale=locale(encoding="Windows-1250"))
+#   data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
+#     strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
+#   data$obcina[data$obcina == "Sveti Jurij"] <- iconv("Sveti Jurij ob Ščavnici", to="UTF-8")
+#   data <- data %>% gather(`1`:`4`, key="velikost.druzine", value="stevilo.druzin")
+#   data$velikost.druzine <- parse_number(data$velikost.druzine)
+#   data$obcina <- parse_factor(data$obcina, levels=obcine)
+#   return(data)
+# }
+# 
+# # Zapišimo podatke v razpredelnico obcine
+# obcine <- uvozi.obcine()
+# 
+# # Zapišimo podatke v razpredelnico druzine.
+# druzine <- uvozi.druzine(levels(obcine$obcina))
+# 
+# # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
+# # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
+# # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
+# # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
+# # fazah.
