08d2a62866ebdd290bcbaa3b29eea4652b7c199b
Lenart11 <49915334+Lenart11@users.noreply.github.com>
2020-01-16 10:01:58 +0100

    Update uvoz.r

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -48,6 +48,38 @@ obcine <- uvozi.obcine()
 # Zapišimo podatke v razpredelnico druzine.
 druzine <- uvozi.druzine(levels(obcine$obcina))
 
+
+uvoz.place <- function(Tabela_place) {
+  Tabela_place <- read_csv2("podatki/earn_nt_net .csv", col_names = TRUE, na=":", locale=locale(encoding="Windows-1250"))
+  Tabela_place <- Tabela_place[ -c(31:43), ]
+  return(Tabela_place)
+}
+
+uvoz.BDP <- function(Tabela_BDP) {
+  Tabela_BDP <- read_xlsx("podatki/BDP_pc.xlsx", na="-")
+  Tabela_BDP <- Tabela_BDP[ -c(31:35), ]
+  return(Tabela_BDP)
+}
+
+uvoz.davki <- function(Tabela_davki) {
+  Tabela_davki <- read_xls("podatki/earn_nt_taxrate.xls", na=":")
+  return(Tabela_davki)
+}
+
+uvoz.starost <- function(Tabela_starost) {
+  link_starost <- "https://en.wikipedia.org/wiki/List_of_countries_by_median_age"
+  stran_starost <- html_session(link_starost) %>% read_html()
+  Tabela_starost <- stran_starost %>% html_nodes(xpath = "//table[@class='wikitable sortable']") %>%
+    .[[1]] %>% html_table(dec=".")
+  Tabela_starost <- Tabela_starost[ ,c("Country/Territory", "Median(Years)")]
+  return(Tabela_starost)
+}
+
+Tabela_place <- uvoz.place()
+Tabela_BDP <- uvoz.BDP()
+Tabela_davki <- uvoz.davki()
+Tabela_starost <- uvoz.starost()
+
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
 # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
