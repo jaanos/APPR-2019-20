1db74392c9912ee851dbb29c339a91fff5d2996c
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2020-09-23 18:15:57 +0200

    Nastavljena projekcija zemljevida
    cf. EnjaErker/APPR-2019-20#4

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -3,6 +3,9 @@
 # Uvozimo zemljevid.
 zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
                              pot.zemljevida="OB", encoding="Windows-1250")
+# Če zemljevid nima nastavljene projekcije, jo ročno določimo
+proj4string(zemljevid) <- CRS("+proj=utm +zone=10+datum=WGS84")
+
 levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
   { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
 zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
