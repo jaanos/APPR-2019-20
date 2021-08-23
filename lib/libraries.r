fe0c45c13780c03c20d28bf21819d7dbaa9b70ca
Lenart11 <49915334+Lenart11@users.noreply.github.com>
2021-08-23 22:35:52 +0200

    Add files via upload

--- lib/libraries.r
+++ lib/libraries.r
@@ -7,6 +7,7 @@ library(readr)
 library(readxl)
 library(dplyr)
 library(ggplot2)
+library(tmap) #za zemljevid sveta
 options(gsubfn.engine="R")
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 #source("lib/uvozi.zemljevid.r", encoding="UTF-8")
