9ab8925bdc6c39ec133b3611031a96c011cf49c3
petertiselj <40868564+petertiselj@users.noreply.github.com>
2021-08-29 00:10:04 +0200

    Add files via upload

--- lib/libraries.r
+++ lib/libraries.r
@@ -1,11 +1,16 @@
+# Ko potrebujemo kakšno knjižnico, do dodamo sem
+
 library(knitr)
 library(rvest)
 library(gsubfn)
 library(tidyr)
-library(tmap)
 library(shiny)
-
+library(readr)
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(tmap) #za zemljevid sveta
 options(gsubfn.engine="R")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
-source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+#source("lib/uvozi.zemljevid.r", encoding="UTF-8")
